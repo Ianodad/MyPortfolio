fa5be5043af9368706eb0253639b4cac419a075c
ianodad <ianodad@gmail.com>
2020-08-22 16:27:56 +0300

    window width again

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ const Header = ({ siteTitle }) => {
   //     marginBottom: `1.45rem`,
   //   }}
   // >
-  const [width, setWidth] = useState(1000);
+  const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     // eslint-disable-next-line no-use-before-define
